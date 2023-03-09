4d14d70b13c99d6473af79e6045e80f6a5c9d2e7
breno.lima <brenoryan21@hotmail.com>
2023-03-09 16:15:24 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/breno/hrpayroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.breno.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.breno.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+	public Payment getPayment(Long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+}
